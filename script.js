aa6be035579d4ed7d5174980fe385ee630c7cd41
Adrianeeeeeee <raniewise14@gmail.com>
2024-11-20 17:45:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function switchToSignup() {
+    $('#loginModal').modal('hide'); // Hide the login modal
+    $('#signupModal').modal('show'); // Show the sign up modal
+}
+
+function switchToLogin() {
+    $('#signupModal').modal('hide '); // Hide the sign up modal
+    $('#loginModal').modal('show'); // Show the login modal
+}
